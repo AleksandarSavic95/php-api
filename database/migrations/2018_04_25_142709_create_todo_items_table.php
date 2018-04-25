9daae2b160b2fec3728e4fa4ae63f2332c18f98b
AleksandarSavic95 <alek_bn@hotmail.com>
2018-04-25 17:15:21 +0200

    #ALSA-14 make TodoItem's model
    - added Priority constants (low, mid, high),
    - tested obtaining a constant value in web.php :)

---
+++ database/migrations/2018_04_25_142709_create_todo_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodoItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+	        $table->string('title', 50);
+	        $table->string('content');
+            $table->integer('priority');
+            $table->boolean('done');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo_items');
+    }
+}
