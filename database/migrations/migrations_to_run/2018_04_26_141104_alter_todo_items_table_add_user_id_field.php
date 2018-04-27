d525f640ef4aab3cffb784c783959402a5869bdf
AleksandarSavic95 <alek_bn@hotmail.com>
2018-04-27 14:14:05 +0200

    #ALSA-14 remove test routes, rename migration by convention

--- database/migrations/migrations_to_run/2018_04_26_141104_alter_todo_items_table_add_user_id_field.php
+++ database/migrations/migrations_to_run/2018_04_26_141104_alter_todo_items_table_add_user_id_field.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UserIdInTodoitems extends Migration
+class AlterTodoItemsTableAddUserIdField extends Migration
 {
     /**
      * Run the migrations.
